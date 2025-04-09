3fa359e4d1cae541790b271f5c1edc61d5bad582
Felhari <prizrahnik24@gmail.com>
2025-04-09 13:29:51 +0500

    dz

--- dz2.py
+++ dz2.py
@@ -1,20 +1,21 @@
-import os
+class car:
+    marka = "X7 m501"
+    year = "2021"
+    car = "BMW"
+    model = "530 л.с "
+    colour = "Белый"
+    price = "1079000 рублей"
+
+    def print_info(self):
+        print(" Данные автомобиля".center(40, "*"))
+        print(f"Название модели:{self.marka}\n"
+              f"Год выпуска:{self.year}\n"
+              f"Производитель:{self.car}\n"
+              f"Мощность ДВС:{self.model}\n"
+              f"Цвет машины:{self.colour}\n"
+              f"Цена:{self.price}")
+        print("=" * 40)
 
 
-def info_files(root, folder):
-    for root, dirs, files in os.walk(root):
-        for file in files:
-            file_path = os.path.join(root, file)
-            print(file_path)
-            file_size = os.path.getsize(file_path)
-            if file_size == 0:
-                os.renames(file_path, os.path.join(folder, file))
-                print(f"Файл {file} перемещен из папки {root} в папку {folder}")
-            else:
-                print(f"{file_path} - {file_size} bytes")
-
-
-info_files("Work", "Work/empty_files")
-
-
-
+r1 = car()
+r1.print_info()
