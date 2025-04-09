0f9e946af3fb56a322caabac6f77e3a3be9eac44
Felhari <prizrahnik24@gmail.com>
2025-04-09 18:09:43 +0500

    dz

--- dz3.py
+++ dz3.py
@@ -1,1 +1,14 @@
-print "hi"
+class WeightConverter:
+    @staticmethod
+    def kg_to_lb(kg):
+        if not isinstance(kg, (int, float)):
+            raise TypeError("Должно быть число")
+        if kg < 0:
+            raise ValueError("Вес не может быть отрицательным")
+        return kg * 2.205
+
+w1=12
+w2=41
+
+print(w1,f" кг = {WeightConverter.kg_to_lb(w1):.2f} фунтов")
+print(w2,f" кг = {WeightConverter.kg_to_lb(w2):.3f} фунтов")
