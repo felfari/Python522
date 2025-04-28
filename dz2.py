0cdc0a36839087b2420080bbf6c1b1cfb5d60c2e
Felhari <prizrahnik24@gmail.com>
2025-04-28 12:09:00 +0500

    dz51

--- dz2.py
+++ dz2.py
@@ -1,65 +1,28 @@
-class Person:
-
-    def __init__(self, name, age):
+class Student:
+    def __init__(self, name, laptop):
         self.name = name
-        self.age = age
+        self.laptop = laptop
 
+    def display_info(self):
+        print(f"Студент => {self.name}")
+        self.laptop.display_info()
 
-    def __str__(self):
-        return f"{self.name}, {self.age} лет"
+class Laptop:
+    def __init__(self, brand, processor, memory):
+        self.brand = brand
+        self.processor = processor
+        self.memory = memory
 
+    def display_info(self):
+        print(f"Ноутбук: {self.brand}, Процессор: {self.processor}, Память: {self.memory} ГБ")
 
-class Pupil(Person):
+# Пример использования
+roman_laptop = Laptop("HP", "i7", 16)
+roman = Student("Роман", roman_laptop)
 
+vladimir_laptop = Laptop("HP", "i7", 16)
+vladimir = Student("Владимир", vladimir_laptop)
 
-    def __init__(self, name, age, group, level):
-        super().__init__(name, age)
-        self.group = group
-        self.level = level
-
-
-    def __str__(self):
-        base_info = super().__str__()
-        return f"{base_info}, учится в группе {self.group}, уровень {self.level}"
-
-
-class Educator(Person):
-
-
-    def __init__(self, name, age, department, level):
-        super().__init__(name, age)
-        self.department = department
-        self.level = level
-
-
-    def __str__(self):
-        base_info = super().__str__()
-        return f"{base_info}, работает в {self.department}, уровень {self.level}"
-
-
-class Alumnus(Person):
-
-
-    def __init__(self, name, age, field, level):
-        super().__init__(name, age)
-        self.field = field
-        self.level = level
-
-
-    def __str__(self):
-        base_info = super().__str__()
-        return f"{base_info}, работает в области {self.field}, уровень {self.level}"
-
-
-members = [
-    Pupil("Батодадаев Даши", 16, "Web_011", 5),
-    Educator("Загдулин Линар", 32, "РПО PD_011", 5),
-    Pupil("Шугани Сергей", 15, "РПО PD_011", 5),
-    Alumnus("Даншин Андрей", 38, "Астрофизика", 110),
-    Pupil("Маркин Даниил", 17, "Python_011", 5),
-    Educator("Баширов Алексей", 45, "Программирование", 20)
-]
-
-
-for member in members:
-    print(member)
+# Вывод информации
+roman.display_info()
+vladimir.display_info()
